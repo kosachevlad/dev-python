8fdb453f60f3411217e3b52861fcdc916a146328
Vlad Kosachev <38155972+kosachevlad@users.noreply.github.com>
2026-09-25 20:59:27 +0300

    feat: extract React AppShell component

---
+++ src/components/AppShell.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AppShell() {
+  return (
+    <main
+      className="react-app-shell"
+      aria-label="Кодик"
+    >
+      <header className="react-header">
+        Кодик
+      </header>
+    </main>
+  );
+}
+
+export default AppShell;

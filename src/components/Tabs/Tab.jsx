5e6169f66f34e8bc8d51cf0884344f751d064c27
Vlad Kosachev <38155972+kosachevlad@users.noreply.github.com>
2026-09-25 21:04:03 +0300

    feat: make React Tab selectable

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Tab({ name, active }) {
+function Tab({ name, active, onClick }) {
   return (
     <button
       type="button"
       className={active ? 'react-tab is-active' : 'react-tab'}
+      onClick={onClick}
     >
       <span>{name}</span>
       <span aria-hidden="true">×</span>
